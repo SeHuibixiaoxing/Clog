4199b77079763592caa4e1551a247eafc8bb7d10
wangz <wangzeyujiuyi@qq.com>
2023-12-21 17:30:35 +0800

    update tools, alu

--- example/verilog/tools.v
+++ example/verilog/tools.v
@@ -1,18 +1,15 @@
 module decoder
 #(
-  parameter IN_WIDTH=5,
-  parameter OUT_WIDTN=32
+  parameter IN_WIDTH=1,
+  parameter OUT_WIDTN=1
 )
 (
-    input  [IN_WIDTH - 1:0] in,
-    output [OUT_WIDTN - 1:0] out
+    input  [IN_WIDTH - 1:0] in_port,
+    output [OUT_WIDTN - 1:0] out_port
 );
 genvar i;
 generate for (i=0; i<OUT_WIDTN; i=i+1) begin: gen_for_dec
-    assign out[i] = (in == i);
+    assign out_port[i] = (in_port == i);
 end endgenerate
 
-
 endmodule
-
-
